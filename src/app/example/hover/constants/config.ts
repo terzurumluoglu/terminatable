fc28639f7bbcb43df9ad9b4472af05d0630e77cc
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2024-04-15 17:25:37 +0300

    hover component

---
+++ src/app/example/hover/constants/config.ts
@@ -0,0 +1,16 @@
+import { IConfig } from "../../../../../projects/terminatable/src/public-api";
+
+export const CONFIG: IConfig = {
+  uniqueField: 'id',
+  strip: true,
+  style: {
+    body: {
+      hover: {
+        color: {
+          background: '#9C5858',
+          text: '#FFFFFF'
+        }
+      }
+    }
+  }
+};
