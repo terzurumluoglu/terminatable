b5677f9035fda3ef41d9e3142c63cb97af383940
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2024-04-17 22:58:19 +0300

    edit

--- src/app/mock/tabs.ts
+++ src/app/mock/tabs.ts
@@ -4,31 +4,31 @@ export const TABS: ITab[] = [
   {
     id: 'preview',
     title: 'Preview',
-    tables: ['basic', 'frozen', 'strip', 'hover', 'row-selection'],
+    tables: ['basic', 'frozen', 'strip', 'hover', 'row-selection', 'color-customize', 'multi-select'],
   },
   {
     id: 'columns',
     title: 'Columns',
-    tables: ['basic', 'frozen', 'strip', 'hover', 'row-selection'],
+    tables: ['basic', 'frozen', 'strip', 'hover', 'row-selection', 'color-customize', 'multi-select'],
   },
   {
     id: 'config',
     title: 'Config',
-    tables: ['strip', 'hover', 'row-selection'], 
+    tables: ['strip', 'hover', 'row-selection', 'color-customize', 'multi-select'],
   },
   {
     id: 'data',
     title: 'Data',
-    tables: ['basic', 'frozen', 'strip', 'hover', 'row-selection'],
+    tables: ['basic', 'frozen', 'strip', 'hover', 'row-selection', 'color-customize', 'multi-select'],
   },
   {
     id: 'html',
     title: 'HTML',
-    tables: ['basic', 'frozen', 'strip', 'hover', 'row-selection'],
+    tables: ['basic', 'frozen', 'strip', 'hover', 'row-selection', 'color-customize', 'multi-select'],
   },
   {
     id: 'event',
     title: 'Event',
-    tables: ['row-selection'],
+    tables: ['row-selection', 'color-customize', 'multi-select'],
   },
 ];
